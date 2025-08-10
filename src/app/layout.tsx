f06402062e2c53206680b5e96d2dd15658577fe4
onlineasd2 <31164176+onlineasd2@users.noreply.github.com>
2025-08-11 00:11:00 +0500

    GMT

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -28,6 +28,17 @@ export default function RootLayout({
   return (
     <html lang="en">
         <head>
+            {/* Google Tag Manager */}
+            <Script id="gtm" strategy="afterInteractive" type="text/plain" data-cookieconsent="statistics">
+              {`
+                (function(w,d,s,l,i){w[l]=w[l]||[];w[l].push({'gtm.start':
+                new Date().getTime(),event:'gtm.js'});var f=d.getElementsByTagName(s)[0],
+                j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';j.async=true;j.src=
+                'https://www.googletagmanager.com/gtm.js?id='+i+dl;f.parentNode.insertBefore(j,f);
+                })(window,document,'script','dataLayer','GTM-TPWRNL5M');
+              `}
+            </Script>
+
             <link rel="preload" href="/logo.webp" as="image" type="image/webp" />
             <link rel="preload" href="/app-ui.webp" as="image" type="image/webp" />
             <link rel="dns-prefetch" href="//fonts.googleapis.com" />
@@ -40,6 +51,11 @@ export default function RootLayout({
             <script id="Cookiebot" src="https://consent.cookiebot.com/uc.js" data-cbid="16a9ac60-a151-41aa-9f0b-7e90d7855310" type="text/javascript" async></script>
         </head>
         <body className={inter.className}>
+            {/* Google Tag Manager (noscript) */}
+            <noscript>
+              <iframe src="https://www.googletagmanager.com/ns.html?id=GTM-TPWRNL5M" height="0" width="0" style={{display:'none',visibility:'hidden'}}></iframe>
+            </noscript>
+
             <a href="#main-content" className="sr-only focus:not-sr-only focus:absolute focus:top-4 focus:left-4 bg-blue-600 text-white px-4 py-2 rounded z-50">
                 Skip to main content
             </a>
